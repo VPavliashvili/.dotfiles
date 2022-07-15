2bcd895374cc6ff1b62e6edc47ba031dc95819fa
VPavliashvili <v_pavliashvili@yahoo.com>
2022-07-15 05:36:25 +0400

    wezterm config added

---
+++ wezterm/.wezterm.lua
@@ -0,0 +1,8 @@
+local wezterm = require 'wezterm';
+return {
+    font = wezterm.font("JetBrains Mono Nerd Font"),
+    color_scheme = "Batman",
+    warn_about_missing_glyphs = false,
+    enable_tab_bar = true,
+    hide_tab_bar_if_only_one_tab = true,
+}
