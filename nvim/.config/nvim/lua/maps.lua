09665d667e26e24f7ce74ca21c0e29f24f2fbc5c
VPavliashvili <v_pavliashvili@yahoo.com>
2022-07-17 02:33:55 +0400

    NvimTree external maps added

--- nvim/.config/nvim/lua/maps.lua
+++ nvim/.config/nvim/lua/maps.lua
@@ -13,4 +13,9 @@ nnoremap <C-j> <C-e>
 
 "hide highlighings
 nnoremap <leader>nh :noh<CR>
+
+"Nvim Tree external maps
+nnoremap ft :NvimTreeFocus<CR>
+nnoremap rt :NvimTreeRefresh<CR>
+nnoremap tt :NvimTreeToggle<CR>
 ]]
