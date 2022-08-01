6c1e4bdb86ceb4137b40263022d8f8a8b0c6e656
VPavliashvili <v_pavliashvili@yahoo.com>
2022-08-01 23:20:14 +0400

    current image exclusion implemented

--- wezterm/.config/wezterm/modules/auto_bgimage_changer.lua
+++ wezterm/.config/wezterm/modules/auto_bgimage_changer.lua
@@ -3,7 +3,6 @@ local M = {}
 local json_handler = require("modules.json_handler")
 
 local home = os.getenv("HOME")
---local config_file = home .. "/.config/wezterm/data.txt"
 local update_wallpaper_interval_in_minutes = 1
 
 local image_names = {
@@ -27,22 +26,20 @@ local function get_timestamp()
     return seconds_since_epoch
 end
 
---local function get_last_saved_date()
-    --local file = io.open(config_file, "r")
-    --io.input(file)
-    --local last_written_time = io.read("*n")
-    --return last_written_time
---end
-
---local function update_saved_date(current_time)
-    --local file = io.open(config_file, "w+")
-    --io.output(file)
-    --io.write(current_time)
-    --io.close(file)
---end
-
 local function get_image_name()
     local current_timestamp = get_timestamp()
+
+    if #image_names == 1 then
+        local images = {}
+        for _, v in pairs(image_names) do
+            table.insert(images, v)
+        end
+		local res = images[1]
+        json_handler.write_to_json_file(res, current_timestamp)
+        print("image returned " .. res)
+        return res
+    end
+
     local saved_data = json_handler.read_from_json_file()
 
     local needUpdate = false
@@ -50,7 +47,7 @@ local function get_image_name()
         needUpdate = true
         print("last saved data is null")
     else
-		local last_timestamp = saved_data.timestamp
+        local last_timestamp = saved_data.timestamp
 
         local diff = current_timestamp - last_timestamp
         needUpdate = (diff / 60) >= update_wallpaper_interval_in_minutes
@@ -63,22 +60,30 @@ local function get_image_name()
     local result
     if needUpdate then
         math.randomseed(os.time())
-        --_ = math.random()
 
-        local keyset = {}
-        for k in pairs(image_names) do
-            table.insert(keyset, k)
+        local images = {}
+        if saved_data ~= nil then
+            for _, v in pairs(image_names) do
+                if v ~= saved_data.image then
+                    table.insert(images, v)
+                end
+            end
+        else
+            for _, v in pairs(image_names) do
+                table.insert(images, v)
+            end
         end
 
-        local index = math.random(#keyset)
+        local index = math.random(#images)
+        print(images)
         print("random index: " .. index)
-        result = image_names[keyset[index]]
+        result = images[index]
 
-		json_handler.write_to_json_file(result, current_timestamp)
+        json_handler.write_to_json_file(result, current_timestamp)
         print("update data function run")
     end
 
-	print(result)
+    print(result)
 
     if result == nil then
         result = saved_data.image
