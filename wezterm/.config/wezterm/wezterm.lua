d9912fc558d2defb7e019e9e3ced15528d84d703
VPavliashvili <v_pavliashvili@yahoo.com>
2022-08-25 01:33:25 +0400

    picom compositor added, slightly configured, but much could be done in future

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -10,12 +10,11 @@ local fonts = {
 return {
     font = wezterm.font(fonts.jetbrains_mono),
     font_size = 11,
-    color_scheme = "MaterialDesignColors",
     warn_about_missing_glyphs = false,
     enable_tab_bar = true,
     use_fancy_tab_bar = false,
+    window_background_opacity = 0.8,
     hide_tab_bar_if_only_one_tab = true,
-    window_background_opacity = 0.5,
     keys = {
         { key = "k", mods = "CTRL|SHIFT", action = act.ScrollByLine(-5) },
         { key = "j", mods = "CTRL|SHIFT", action = act.ScrollByLine(5) },
