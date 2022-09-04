459c2416e1993dea6e672ccd535a15a306f46009
VPavliashvili <v_pavliashvili@yahoo.com>
2022-09-04 22:19:20 +0400

    i3lock riced up

--- bin/bin/lock.sh
+++ bin/bin/lock.sh
@@ -1,16 +1,9 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-# try to delete previous screen if exist
+ICON=~/Pictures/icons/key-fancy.png
+TMPBG=~/temp/lock.png
+scrot ~/temp/lock.png
+convert $TMPBG -scale 10% -scale 1000% $TMPBG
+convert $TMPBG $ICON -gravity center -composite -matte $TMPBG
+i3lock -i $TMPBG --indicator --radius 120 --ring-width 15.0 --inside-color=00000000 --ring-color=E88E41 --keyhl-color=55B4C0 --greeter-color=92BF7B --greeter-text="Provide a Password" --wrong-text="" --noinput-text="" --verif-text="" --line-uses-ring  --greeter-font='Roboto:style=Medium Italic'
 rm ~/temp/lock.png
-
-flameshot full -p ~/temp/lock.png
-
-icon="$HOME/Pictures/icons/password.png"
-tmpbg="$HOME/temp/lock.png"
-
-(( $# )) && { icon=$1; }
-
-scrot "$tmpbg"
-convert "$tmpbg" -scale 10% -scale 1000% "$tmpbg"
-convert "$tmpbg" "$icon" -gravity center -composite -matte "$tmpbg"
-i3lock -u -i "$tmpbg"
