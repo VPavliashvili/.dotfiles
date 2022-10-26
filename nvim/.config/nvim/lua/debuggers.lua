7fdb4749f7aec9eed6279146bb48b088ea1e79e6
VPavliashvili <v_pavliashvili@yahoo.com>
2022-10-27 01:32:01 +0400

    csharp basic debbuger added

--- nvim/.config/nvim/lua/debuggers.lua
+++ nvim/.config/nvim/lua/debuggers.lua
@@ -54,3 +54,22 @@ dap.configurations.lua = {
 dap.adapters.nlua = function(callback, config)
     callback({ type = 'server', host = config.host, port = config.port })
 end
+
+
+
+dap.adapters.coreclr = {
+  type = 'executable',
+  command = '/usr/bin/netcoredbg',
+  args = {'--interpreter=vscode'}
+}
+
+dap.configurations.cs = {
+  {
+    type = "coreclr",
+    name = "launch - netcoredbg",
+    request = "launch",
+    program = function()
+        return vim.fn.input('Path to dll', vim.fn.getcwd() .. '/bin/Debug/', 'file')
+    end,
+  },
+}
