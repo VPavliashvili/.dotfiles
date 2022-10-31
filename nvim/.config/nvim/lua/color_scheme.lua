4ff4d1ce38bf336e53c254ff2b29304c41f1ba71
VPavliashvili <v_pavliashvili@yahoo.com>
2022-10-31 23:11:09 +0400

    LineNr color changed

--- nvim/.config/nvim/lua/color_scheme.lua
+++ nvim/.config/nvim/lua/color_scheme.lua
@@ -4,10 +4,14 @@ require('onedark').setup {
 
     colors = {
         local_variable = "#ef5f6b",
+        line_number = "#2D6D90",
+        line_number_highlighted = "#A5D4DC"
     },
 
     highlights = {
-        ["@variable"] = {fg = '$local_variable'},
+        ["@variable"] = { fg = '$local_variable' },
+        LineNr = { fg = '$line_number' },
+        CursorLineNr = { fg = '$line_number_highlighted' }
     },
 }
 
