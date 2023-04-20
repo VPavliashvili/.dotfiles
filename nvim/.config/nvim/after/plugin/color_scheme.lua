fdf0881057e69491c8498d0805cad2cd15df134b
VPavliashvili <v_pavliashvili@yahoo.com>
2023-04-20 23:25:14 +0400

    go syntax highlighting fixed to my taste

--- nvim/.config/nvim/after/plugin/color_scheme.lua
+++ nvim/.config/nvim/after/plugin/color_scheme.lua
@@ -1,11 +1,16 @@
 require("onedarkpro").setup({
     colors = {
-        line_number = "#2D6D90",
-        line_number_highlighted = "#A5D4DC",
-        nvim_tree_root_folder = "#e0a63b",
-        nvim_tree_folder_name = "#4f9ac6",
-        nvim_tree_folder_icon = "#4f9ac6",
+        line_number                 = "#2D6D90",
+        line_number_highlighted     = "#A5D4DC",
+        nvim_tree_root_folder       = "#e0a63b",
+        nvim_tree_folder_name       = "#4f9ac6",
+        nvim_tree_folder_icon       = "#4f9ac6",
         nvim_tree_openedFolder_name = "#95e8e5",
+        red                         = "#e06c75",
+        blue                        = "#61afef",
+        cyan                        = "#56b6c2",
+        yellow                      = "#e5c07b",
+        purple                      = "#c678dd",
     }, -- Override default colors or create your own
     highlights = {
         LineNr = { fg = "${line_number}" },
@@ -14,23 +19,34 @@ require("onedarkpro").setup({
         NvimTreeFolderName = { fg = "${nvim_tree_folder_name}" },
         NvimTreeFolderIcon = { fg = "${nvim_tree_folder_name}" },
         NvimTreeOpenedFolderName = { fg = "${nvim_tree_openedFolder_name}" },
+        -- go modification
+        ["@constant.go"] = { fg = "${red}" },
+        ["@function.call.go"] = { fg = "${blue}" },
+        ["@method.call.go"] = { fg = "${blue}" },
+        ["@function.builtin.go"] = { fg = "${cyan}" },
+        ["@operator.go"] = { fg = "${cyan}" },
+        ["@type.go"] = { fg = "${yellow}" },
+        ["@type.builtin.go"] = { fg = "${purple}" },
+        ["@field.go"] = { fg = "${red}" },
+        ["@property.go"] = { fg = "${red}" },
+        ["@variable.go"] = { fg = "${red}" },
+        ["@parameter.go"] = { fg = "${red}" },
     }, -- Override default highlight groups or create your own
-    filetypes = { -- Override which filetype highlight groups are loaded
+    filetypes = {
+        -- Override which filetype highlight groups are loaded
         -- See the 'Configuring filetype highlights' section for the available list
-        go = true,
     },
     plugins = { -- Override which plugin highlight groups are loaded
         -- See the 'Supported plugins' section for the available list
     },
     options = {
-        bold = true, -- Use bold styles?
-        italic = true, -- Use italic styles?
-        underline = true, -- Use underline styles?
-        undercurl = true, -- Use undercurl styles?
-
-        cursorline = true, -- Use cursorline highlighting?
-        transparency = true, -- Use a transparent background?
-        terminal_colors = true, -- Use the theme's colors for Neovim's :terminal?
+        bold = true,                        -- Use bold styles?
+        italic = true,                      -- Use italic styles?
+        underline = true,                   -- Use underline styles?
+        undercurl = true,                   -- Use undercurl styles?
+        cursorline = true,                  -- Use cursorline highlighting?
+        transparency = true,                -- Use a transparent background?
+        terminal_colors = true,             -- Use the theme's colors for Neovim's :terminal?
         highlight_inactive_windows = false, -- When the window is out of focus, change the normal background?
     }
 })
