fc23ef06fa1a4b21c50ec3067e615d788e0a3ce2
VPavliashvili <v_pavliashvili@yahoo.com>
2023-06-12 07:41:51 +0400

    hyprland working configuration added

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -8,12 +8,15 @@ return {
     use_fancy_tab_bar = false,
     window_background_opacity = 0.8,
     hide_tab_bar_if_only_one_tab = true,
-    font = wezterm.font_with_fallback{
+    enable_wayland = true,
+    font = wezterm.font_with_fallback {
         'JetBrainsMono Nerd Font',
         'Hack Nerd Font Mono',
         'Hack Nerd Font Propo',
-        'DroidSansM Nerd Font',
+        'Font Awesome 6 Free Solid',
+        'Font Awesome 6 Brands 10',
         'Font Awesome 6 Free',
+        'Noto Color Emoji',
     },
     keys = {
         { key = "k", mods = "CTRL|SHIFT", action = act.ScrollByLine(-5) },
