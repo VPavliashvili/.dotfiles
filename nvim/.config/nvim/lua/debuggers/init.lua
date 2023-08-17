fe52d14df2dd63af444b0fa3e4fcc041a6032eb1
VPavliashvili <v_pavliashvili@yahoo.com>
2023-08-18 01:28:05 +0400

    basic csharp debugging configured

--- nvim/.config/nvim/lua/debuggers/init.lua
+++ nvim/.config/nvim/lua/debuggers/init.lua
@@ -55,30 +55,57 @@ dap.adapters.nlua = function(callback, config)
     callback({ type = 'server', host = config.host, port = config.port })
 end
 
-local home = os.getenv("HOME")
-local netcoredbgcommang = home .. "/.local/share/nvim/mason/bin/netcoredbg"
-
+-- c#
 dap.adapters.coreclr = {
     type = "executable",
-    -- command = "/usr/bin/netcoredbg",
-    command = netcoredbgcommang,
+    command = "/usr/bin/netcoredbg",
     args = { "--interpreter=vscode" },
 }
 
-dap.adapters.netcoredbg = {
-    type = "executable",
-    -- command = "/usr/bin/netcoredbg",
-    command = netcoredbgcommang,
-    args = { "--interpreter=vscode" },
-}
+vim.g.dotnet_build_project = function()
+    local default_path = vim.fn.getcwd() .. '/'
+    if vim.g['dotnet_last_proj_path'] ~= nil then
+        default_path = vim.g['dotnet_last_proj_path']
+    end
+    local path = vim.fn.input('Path to your *proj file: ', default_path, 'file')
+    vim.g['dotnet_last_proj_path'] = path
+    local cmd = 'dotnet build -c Debug "' .. path .. '" 2> /tmp/dap-debug-nvim-dotnet.log'
+    print('\n')
+    print('Cmd to execute: ' .. cmd)
+    local f = os.execute(cmd)
+    if f == 0 then
+        print('\nBuild: ' .. vim.g.gsign('✔️ ', 'OK'))
+    else
+        print('\nBuild: ' .. vim.g.gsign('❌', 'ERR') .. '(code: ' .. f .. ')')
+    end
+end
+
+vim.g.dotnet_get_dll_path = function()
+    local request = function()
+        return vim.fn.input('Path to dll: ', vim.fn.getcwd() .. '/bin/Debug/', 'file')
+    end
+
+    if vim.g['dotnet_last_dll_path'] == nil then
+        vim.g['dotnet_last_dll_path'] = request()
+    else
+        if vim.fn.confirm('Do you want to change the path to dll?\n' .. vim.g['dotnet_last_dll_path'], '&yes\n&no', 2) == 1 then
+            vim.g['dotnet_last_dll_path'] = request()
+        end
+    end
+    return vim.g['dotnet_last_dll_path']
+end
 
 dap.configurations.cs = {
     {
         type = "coreclr",
         name = "launch - netcoredbg",
         request = "launch",
+        console = "integratedTerminal",
         program = function()
-            return vim.fn.input("Path to dll: ", vim.fn.getcwd() .. "/bin/Debug/", "file")
+            if vim.fn.confirm('Should I recompile first?', '&yes\n&no', 2) == 1 then
+                vim.g.dotnet_build_project()
+            end
+            return vim.g.dotnet_get_dll_path()
         end,
     },
 }
