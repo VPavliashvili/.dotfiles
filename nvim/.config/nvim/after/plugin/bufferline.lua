522a38842a2b89a8dfcb86aec1c7b34f26483117
VPavliashvili <v_pavliashvili@yahoo.com>
2023-10-06 18:48:22 +0400

    hyprland animations disabled and buffer jumping keymaps added

--- nvim/.config/nvim/after/plugin/bufferline.lua
+++ nvim/.config/nvim/after/plugin/bufferline.lua
@@ -37,6 +37,18 @@ nnoremap <silent><S-h> :BufferLineCyclePrev<CR>
 "move tab/buffer physically
 nnoremap <silent><A-l> :BufferLineMoveNext<CR>
 nnoremap <silent><A-h> :BufferLineMovePrev<CR>
+
+"jump to the buffer number from 1 up to 10
+nnoremap <silent><A-1> <Cmd>BufferLineGoToBuffer 1<CR>
+nnoremap <silent><A-2> <Cmd>BufferLineGoToBuffer 2<CR>
+nnoremap <silent><A-3> <Cmd>BufferLineGoToBuffer 3<CR>
+nnoremap <silent><A-4> <Cmd>BufferLineGoToBuffer 4<CR>
+nnoremap <silent><A-5> <Cmd>BufferLineGoToBuffer 5<CR>
+nnoremap <silent><A-6> <Cmd>BufferLineGoToBuffer 6<CR>
+nnoremap <silent><A-7> <Cmd>BufferLineGoToBuffer 7<CR>
+nnoremap <silent><A-8> <Cmd>BufferLineGoToBuffer 8<CR>
+nnoremap <silent><A-9> <Cmd>BufferLineGoToBuffer 9<CR>
+nnoremap <silent><A-0> <Cmd>BufferLineGoToBuffer 10<CR>
 ]]
 
 require('close_buffers').setup({})
