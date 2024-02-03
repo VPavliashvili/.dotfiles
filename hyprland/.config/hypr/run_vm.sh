425229bd5af19a4d75f10b06ef50748cd6be7171
VPavliashvili <v_pavliashvili@yahoo.com>
2024-02-03 15:07:30 +0400

    virtual machine start from bash implemented

---
+++ hyprland/.config/hypr/run_vm.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+vm_name=""
+gui=false
+wn=""
+
+# -n -> name of the target virtual machine
+# -g -> graphical(otherwise its passthrouged so no need host gui)
+# -w -> target workspace number
+while getopts 'n:w:g' flag
+do
+    case "${flag}" in
+        g) gui=true ;;
+        n) vm_name="${OPTARG}" ;;
+        w) wn="${OPTARG}" ;;
+    esac
+done
+
+echo $gui
+echo $vm_name
+echo $wn
+
+sudo virsh --connect qemu:///system start $vm_name
+
+if [ "$gui" = true ] ; then
+    # virt-manager --connect qemu:///system --show-domain-console $vm_name
+
+    virt-viewer --connect qemu:///system -f -w -a $vm_name --hotkeys=release-cursor=ctrl+alt & disown
+
+    # sleep to wait for hyprland window creation
+    sleep 1
+
+    address=$(hyprctl -j clients | jq -r ".[] | select(.title | contains(\"$vm_name\")) | .address")
+
+    hyprctl dispatch movetoworkspacesilent $wn,address:$address
+    echo "hyprland -> virtual machine ${vm_name} started with window address: ${address} in workspace ${wn}"
+fi
