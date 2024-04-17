dbfa710b7a47abbaa9fafa1804feca081d3a98be
VPavliashvili <v_pavliashvili@yahoo.com>
2024-04-17 23:07:37 +0400

    run_vm script improved

--- hyprland/.config/hypr/run_vm.sh
+++ hyprland/.config/hypr/run_vm.sh
@@ -3,35 +3,46 @@
 vm_name=""
 gui=false
 wn=""
+fullscreen=false
+fakefullscreen=false
 
 # -n -> name of the target virtual machine
 # -g -> graphical(otherwise its passthrouged so no need host gui)
 # -w -> target workspace number
-while getopts 'n:w:g' flag
+# -f -> open windows as fullscreen
+# -k -> start window as fakefullscreen
+while getopts 'n:w:gfk' flag
 do
     case "${flag}" in
         g) gui=true ;;
         n) vm_name="${OPTARG}" ;;
         w) wn="${OPTARG}" ;;
+        f) fullscreen=true ;;
+        k) fakefullscreen=true
     esac
 done
 
 echo $gui
 echo $vm_name
 echo $wn
+echo $fullscreen
+echo $fakefullscreen
 
 virsh --connect qemu:///system start $vm_name
 
 if [ "$gui" = true ] ; then
     # virt-manager --connect qemu:///system --show-domain-console $vm_name
 
-    virt-viewer --connect qemu:///system -f -w -a $vm_name --hotkeys=release-cursor=ctrl+alt & disown
+    if [ "$fakefullscreen" = true ] ; then
+        hyprctl dispatch exec "[workspace $wn silent;fakefullscreen]" "virt-viewer --connect qemu:///system -w -a $vm_name --hotkeys=release-cursor=ctrl+alt"
+        echo "run as fakefullscreen on workspace $wn"
+    elif [ "$fullscreen" = true ] ; then
+        hyprctl dispatch exec "[workspace $wn silent]" "virt-viewer --connect qemu:///system -f -w -a $vm_name --hotkeys=release-cursor=ctrl+alt"
+        echo "run as fullscreen on workspace $wn"
+    else
+        hyprctl dispatch exec "[workspace $wn silent]" "virt-viewer --connect qemu:///system -w -a $vm_name --hotkeys=release-cursor=ctrl+alt"
+        echo "run on workspace $wn"
+    fi
 
-    # sleep to wait for hyprland window creation
-    sleep 1
-
-    address=$(hyprctl -j clients | jq -r ".[] | select(.title | contains(\"$vm_name\")) | .address")
-
-    hyprctl dispatch movetoworkspacesilent $wn,address:$address
     echo "hyprland -> virtual machine ${vm_name} started with window address: ${address} in workspace ${wn}"
 fi
