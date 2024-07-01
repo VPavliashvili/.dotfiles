5f5859ead875078f213f607a752725881bd5988f
VPavliashvili <v_pavliashvili@yahoo.com>
2024-07-01 21:10:23 +0400

    config adjusted to hyprland version change

--- hyprland/.config/hypr/run_vm.sh
+++ hyprland/.config/hypr/run_vm.sh
@@ -1,22 +1,25 @@
 #!/bin/sh
 
 vm_name=""
-gui=false
+gui=-1 # (can be 0, 1, -1. i.e 0 -> default, 1 -> looking-glass, -1 -> no gui)
 wn=""
 fullscreen=false
 fakefullscreen=false
+additional=""
 
 # -n -> name of the target virtual machine
-# -g -> graphical(otherwise its passthrouged so no need host gui)
+# -g -> gui
 # -w -> target workspace number
 # -f -> open windows as fullscreen
 # -k -> start window as fakefullscreen
-while getopts 'n:w:gfk' flag
+# -a -> additional arguments
+while getopts 'n:w:g:a:fk' flag
 do
     case "${flag}" in
-        g) gui=true ;;
+        g) gui="${OPTARG}" ;;
         n) vm_name="${OPTARG}" ;;
         w) wn="${OPTARG}" ;;
+        a) additional="${OPTARG}" ;;
         f) fullscreen=true ;;
         k) fakefullscreen=true
     esac
@@ -30,7 +33,7 @@ echo $fakefullscreen
 
 virsh --connect qemu:///system start $vm_name
 
-if [ "$gui" = true ] ; then
+if [ "$gui" = 0 ] ; then
     # virt-manager --connect qemu:///system --show-domain-console $vm_name
 
     if [ "$fakefullscreen" = true ] ; then
@@ -45,4 +48,17 @@ if [ "$gui" = true ] ; then
     fi
 
     echo "hyprland -> virtual machine ${vm_name} started with window address: ${address} in workspace ${wn}"
+elif [ "$gui" = 1 ] ; then
+    if [ "$fakefullscreen" = true ] ; then
+        hyprctl dispatch exec "[workspace $wn silent;fakefullscreen]" "looking-glass-client ${additional}"
+        echo "run as fakefullscreen on workspace $wn"
+    elif [ "$fullscreen" = true ] ; then
+        hyprctl dispatch exec "[workspace $wn silent]" "looking-glass-client -F ${additional}"
+        echo "run as fullscreen on workspace $wn"
+    else
+        hyprctl dispatch exec "[workspace $wn silent]" "looking-glass-client ${additional}"
+        echo "run on workspace $wn"
+    fi
+
+    echo "vm command -> looking-glass-client ${additional}"
 fi
--- hyprland/.config/hypr/run_work_layout.sh
+++ hyprland/.config/hypr/run_work_layout.sh
@@ -3,7 +3,7 @@
 hyprctl dispatch exec "[workspace 10 silent]" "foot btop" 
 hyprctl dispatch exec spotify
 hyprctl dispatch exec webcord
-hyprctl dispatch exec -- ~/.config/hypr/run_vm.sh -n linux_teams -g -w 7 -k --
-hyprctl dispatch exec -- ~/.config/hypr/run_vm.sh -n linux_remmina -g -w 6 -f --
+hyprctl dispatch exec -- ~/.config/hypr/run_vm.sh -n linux_teams -g 0 -w 7 -k --
+hyprctl dispatch exec -- ~/.config/hypr/run_vm.sh -n linux_remmina -g 0 -w 6 -f --
 hyprctl dispatch exec brave
 hyprctl dispatch exec "[workspace 1 silent]" "foot -e zellij attach main-terminal" 
