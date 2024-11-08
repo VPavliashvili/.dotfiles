b7160b6e856ba08d93e74baafffeb09dccfd8577
VPavliashvili <v_pavliashvili@yahoo.com>
2024-11-08 23:19:11 +0400

    work layout added and home layout improved

--- sway/.config/sway/startup_work.sh
+++ sway/.config/sway/startup_work.sh
@@ -27,3 +27,11 @@ sleep 0.25
 
 swaymsg move scratchpad
 swaymsg 'workspace --no-auto-back-and-forth 1'
+
+sleep 1.5
+
+swaymsg exec '~/.config/sway/run_vm.sh -n win10_liberty_work -w 6 -f -a "-f /dev/shm/looking-glass-work win:showFPS=yes -c 127.0.0.1 -p 5906"'
+
+sleep 1.5
+
+swaymsg exec '~/.config/sway/run_vm.sh -n win10_teams -w 7 -a "-f /dev/shm/looking-glass-teams audio:micDefault=allow win:showFPS=yes -c 127.0.0.1 -p 5907"'
