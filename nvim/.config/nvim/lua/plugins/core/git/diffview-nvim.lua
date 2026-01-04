f0c9ecf70952625b62fdd41cf0b530c62d2b9ddf
VPavliashvili <v_pavliashvili@yahoo.com>
2026-01-04 21:15:54 +0400

    gitgraph tab based flow fix

--- nvim/.config/nvim/lua/plugins/core/git/diffview-nvim.lua
+++ nvim/.config/nvim/lua/plugins/core/git/diffview-nvim.lua
@@ -4,13 +4,35 @@ local function config()
     end)
 
     vim.keymap.set("n", "<leader>gc", function()
-        vim.cmd("DiffviewClose")
+        local focus_on_diffview = false
+        local diffview_buf_prefix = "diffview:"
 
-        if vim.g.user_counter and vim.g.user_counter > 0 then
+        local focus_on_gitgraph = false
+        local gitgraph_buf_postfix = "GitGraph"
+
+        local windows = vim.api.nvim_tabpage_list_wins(0)
+        for _, win in ipairs(windows) do
+            local bufnr = vim.api.nvim_win_get_buf(win)
+            local filepath = vim.api.nvim_buf_get_name(bufnr)
+            if #diffview_buf_prefix <= #filepath and filepath:sub(1, #diffview_buf_prefix) == diffview_buf_prefix then
+                focus_on_diffview = true
+                break
+            end
+            if #gitgraph_buf_postfix <= #filepath and filepath:sub(- #gitgraph_buf_postfix) == gitgraph_buf_postfix then
+                focus_on_gitgraph = true
+                break
+            end
+        end
+
+        if focus_on_diffview then
+            vim.cmd("DiffviewClose")
+            return
+        end
+
+        if focus_on_gitgraph then
             local tab_no = vim.fn.tabpagenr()
             vim.cmd(tab_no .. "tabclose!")
-
-            vim.g.user_counter = vim.g.user_counter - 1
+            return
         end
     end)
 
