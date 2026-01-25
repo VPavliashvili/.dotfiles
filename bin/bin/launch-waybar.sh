ef2c6e5d64e28a83df5a17f41dc15111aeca1b8b
VPavliashvili <v_pavliashvili@yahoo.com>
2026-01-25 05:55:46 +0400

    zjstatus added, scripts dir introduced and some random changes

--- bin/bin/launch-waybar.sh
+++ bin/bin/launch-waybar.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
 CONFIG_FILES="$HOME/.config/waybar/config $HOME/.config/waybar/style.css"
+waybar
 
-trap "killall waybar" EXIT
-
-while true; do
-    waybar &
-    inotifywait -e create,modify $CONFIG_FILES
-    killall waybar
-done
+# trap "killall waybar" EXIT
+#
+# while true; do
+#     waybar &
+#     inotifywait -e create,modify $CONFIG_FILES
+#     killall waybar
+# done
